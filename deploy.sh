b372b7b92c6428f003e134799551e18ae1b52e5e
Jonatan <jsantanamay@gmail.com>
2024-02-20 13:54:33 -0600

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bashs
+
+sudo systemctl stop nginx
+
+cd resume/
+
+git pull origin master
+
+cp -r /home/user/resume/dist/* /var/www/html/
+
+sudo systemctl start nginx
+
+ngrok http http://localhost:80
